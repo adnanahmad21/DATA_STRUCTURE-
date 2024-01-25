0b2911641b7eaba5f63b8d30e45ab03d93382d89
_.adnan21._ <adnan21122001@gmail.com>
2024-01-25 14:09:43 +0530

    Adding Exit
    Adding System.exit() to avoid checking outer condition if inner become false

--- STACKS/balancedBrackets.java
+++ STACKS/balancedBrackets.java
@@ -1,22 +1,27 @@
 import java.util.Stack;
+
 public class balancedBrackets {
     public static void main(String args[]) {
-        String s = "(()())";
-    
+        String s = "(())())";
+
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(')
                 st.push(s.charAt(i));
             else {
                 if (s.charAt(i) == ')')
-                    if (st.isEmpty())
+                    if (st.isEmpty()){
                         System.out.println("Not balanced");
-                        else 
+                        System.exit(0);
+                    }
+                    else
                         st.pop();
             }
         }
-        if(st.isEmpty())   System.out.println(" balanced");
-        else   System.out.println("Not balanced");
+        if (st.isEmpty())
+            System.out.println(" balanced");
+        else
+            System.out.println("Not balanced");
 
     }
 }
