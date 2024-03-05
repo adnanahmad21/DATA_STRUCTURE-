4bc36487216b12f9860988fb0cabe5b020db8054
_.adnan21._ <adnan21122001@gmail.com>
2024-03-05 22:15:43 +0530

    Some Stack questions and concept
    concept of next greater element  is used in various prblems like  sliding window,trapping rainwater,histogram area also to give soln in O(n) TC.

---
+++ STACKS/postfixToinfix.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class postfixToinfix {
+    public static void main(String[] args) {
+        String postfix = "953+4*6/-";
+        Stack<String> val = new Stack<>();
+        for (int i = 0; i < postfix.length(); i++) {
+            char ch = postfix.charAt(i);
+            int ascii = (int) ch;
+            if (ascii >= 48 && ascii <= 57) {
+                String st = "" + ch;
+                val.push((st));
+            } else if (ch == '+') {
+                String v2 = val.pop();
+                String v1 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '-') {
+                String v2 = val.pop();
+                String v1 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '*') {
+                String v2 = val.pop();
+                String v1 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '/') {
+                String v2 = val.pop();
+                String v1 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            }
+        }
+        System.out.println(val.peek());
+    }
+}
---
+++ STACKS/prefixToinfix.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+public class prefixToinfix {
+    public static void main(String[] args) {
+        String prefix = "-9/*+5346";
+        Stack<String> val = new Stack<>();
+        for (int i = prefix.length()-1; i>=0; --i) {
+            char ch = prefix.charAt(i);
+            int ascii = (int) ch;
+            if (ascii >= 48 && ascii <= 57) {
+                String st = "" + ch;
+                val.push((st));
+            } else if (ch == '+') {
+                String v1 = val.pop();
+                String v2 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '-') {
+                String v1 = val.pop();
+                String v2 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '*') {
+                String v1 = val.pop();
+                String v2 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            } else if (ch == '/') {
+                String v1 = val.pop();
+                String v2 = val.pop();
+                String st = '(' + v1 + ch + v2 + ')';
+                val.push(st);
+            }
+        }
+        System.out.println(val.peek());
+    }
+}
