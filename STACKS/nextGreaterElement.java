af4091447bdf0e7bb7610d5f81b60dbe716eaeed
_.adnan21._ <adnan21122001@gmail.com>
2024-03-24 21:13:42 +0530

    Some important concepts
    Here i solve various data structures implementation in java

--- STACKS/nextGreaterElement.java
+++ STACKS/nextGreaterElement.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class nextGreaterElement {
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
-        int a[] = {1,5,3,2,1,6,3,4 };
+        int a[] = { 1, 5, 3, 2, 1, 6, 3, 4 };
         int nge[] = new int[a.length];
         nge[a.length - 1] = -1;
-        st.push(a[a.length-1]);
+        st.push(a[a.length - 1]);
         for (int i = a.length - 2; i >= 0; i--) {
             while (st.size() > 0 && st.peek() < a[i])
                 st.pop();
